df7ccf1176714ac4511d462c5d3c32e1fa060103
Nicholas Muturi <nicholasmuturi1@gmail.com>
2020-01-20 00:43:22 +0300

    [chore|feat] added project section with projects, FEAT-> added responsiveness for icons with custom width

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     $.scrollIt();
 
+    // $(".owl-carousel").owlCarousel({
+    //     loop:true,
+    //     margin:10,
+    //     autoplay:true,
+    //     autoplayTimeout:3000,
+    //     autoplayHoverPause:true
+    // });
+
     $(window).scroll(function(){
         $('nav').toggleClass('fixed-top',$(this).scrollTop() > 700);
     });
