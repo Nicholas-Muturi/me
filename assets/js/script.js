9561da1cb28be5a2856927cfa059ec761c33aa3b
Nicholas Muturi <nicholasmuturi1@gmail.com>
2020-01-21 17:03:28 +0300

    [style] changed font sizes, recommendation by cyrus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
     $.scrollIt();
 
-    // $(".owl-carousel").owlCarousel({
-    //     loop:true,
-    //     margin:10,
-    //     autoplay:true,
-    //     autoplayTimeout:3000,
-    //     autoplayHoverPause:true
-    // });
-
     $(window).scroll(function(){
         $('nav').toggleClass('fixed-top',$(this).scrollTop() > 700);
     });
